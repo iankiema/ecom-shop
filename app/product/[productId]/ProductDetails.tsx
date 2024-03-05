ffc2bf66354f3a78a7d68ab008484f7d022de083
iankiema <kiemaian.ik@gmail.com>
2024-03-05 19:58:38 +0300

    Add cart functionality and product details

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -3,8 +3,9 @@ import Button from "@/app/components/Button"
 import ProductImage from "@/app/components/products/ProductImage"
 import SetColor from "@/app/components/products/SetCollor"
 import SetQuantity from "@/app/components/products/SetQuantity"
+import { useCart } from "@/hooks/useCart"
 import { Rating } from "@mui/material"
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 interface ProductDetailsProps{
     product: any
@@ -33,6 +34,9 @@ const Horizontal = () => {
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({product}) => {
 
+    const {handleAddProductToCart, cartProducts} = useCart();
+    const [isProductInCart, setIsProductInCart] = useState(false);
+    
     const [cartProduct, setCartProduct] = useState<CartProductType>(
         {
             id: product.id,
@@ -44,9 +48,21 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({product}) => {
             quantity: 1,
             price: product.price
         }
-    )
+    );
 
-    console.log(cartProduct)
+    console.log(cartProducts)
+
+    useEffect(() => {
+        setIsProductInCart(false)
+
+        if (cartProducts) {
+            const existingIndex = cartProducts.findIndex((item) => item.id === product.id)
+
+            if (existingIndex > -1) {
+                setIsProductInCart(true)
+            }
+        }
+    }, [cartProducts])
 
     const productRating = 
     product.reviews.reduce((acc:number,item:any) => item.rating + acc,0)/ product.reviews.length;
@@ -95,6 +111,14 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({product}) => {
                     {product.inStock ? 'In stock' : 'Out of stock'}
                 </div>
                 <Horizontal/>
+                {isProductInCart ? (
+                <>
+                    <p>
+                        <span>Product added to cart</span>
+                    </p>
+                </>
+                ) : (
+                <>
                 <SetColor
                     cartProduct={cartProduct}
                     images={product.images}
@@ -106,9 +130,10 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({product}) => {
                 <div className="max-w-[300px]">
                     <Button 
                         label="Add To Cart"
-                        onClick={() => {}}
+                        onClick={() => handleAddProductToCart(cartProduct)}
                     />
                 </div>
+                </>)}
                 <Horizontal/>
             </div>
         </div>
--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,16 +1,51 @@
-import { createContext, useState } from "react";
+import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import { createContext, useCallback, useContext, useState } from "react";
 
 type CartContextType  = {
-    cartTotalQty:number
+    cartTotalQty:number;
+    cartProducts: CartProductType[] | null;
+    handleAddProductToCart: (product: CartProductType) => void
 }
 
 export const CartContext = createContext<CartContextType | null>(null)
 
-export const CartContextProvider = () => {
+interface Props {
+    [propName: string]: any;
+}
 
-    const [cartTotalQty, setCartTotalQty] = useState()
+export const CartContextProvider = (props: Props) => {
+
+    const [cartTotalQty, setCartTotalQty] = useState(0)
+    const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null);
+
+    const handleAddProductToCart = useCallback((product: CartProductType) => {
+        setCartProducts((prev) => {
+            let updatedCart;
+
+            if (prev) {
+                updatedCart = [...prev, product]
+            } else {
+                updatedCart = [product]
+            }
+
+            return updatedCart;
+        })
+    }, [])
+
     const value = {
-        cartTotalQty
+        cartTotalQty,
+        cartProducts,
+        handleAddProductToCart,
     }
-    return <CartContext.Provider value={value}/>
+    return <CartContext.Provider value={value} {...props}/>
+}
+
+export const useCart = () => {
+    const context = useContext(CartContext);
+
+    if(context === null) {
+        throw new Error("useCart must be used within a CartContext Provider")
+    }
+
+    return context
 }
